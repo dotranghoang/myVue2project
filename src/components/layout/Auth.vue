e4a499a98b7b745ff10e701532de804e3eaa7bb5
hi <hoang>
2021-06-30 13:44:57 +0700

    create router

---
+++ src/components/layout/Auth.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-nav>
+      <ul>
+        <li><a href="/login">Login</a></li>
+        <li><a href="/register">Register</a></li>
+      </ul>
+    </b-nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Auth"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import router from './router/index';
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.config.productionTip = false
+  ;
+Vue.config.productionTip = false;
 Vue.use(BootstrapVue, IconsPlugin);
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+});
